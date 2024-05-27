e96329f8fca1c3848c90c2ed566391e240f7afb5
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-05-27 20:16:27 +0000

    commut

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState ("");
+	const [tasks, setTasks] = useState ([]);
+	
+	function addTask (e) {
+		//setTasks([...tasks, task])
+		if (e.key === "Enter") {
+			setTasks([...tasks, task])
+		}
+	};
+
+	function removeTask (itemToRemove) {
+		setTasks(tasks.filter((item) => item != itemToRemove))
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center mt-5">Todolist</h1>
+			<div className="card">
+				<div className="input-group">
+					<input onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => addTask(e)} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="What needs to be done"/>
+				</div>
+				<div className="ms-3">
+					{tasks.map((item, index) => {
+						return (
+							<div className="d-flex justify-content-between" key={item + index}>
+								<li>{item}</li>
+								<button className="btn me-3" onClick={() => removeTask(item)} >
+									<i className="fas fa-window-close text-danger"></i>
+								</button>
+							</div>
+						)
+						
+					})}
+				</div>
+				<div className="card-footer">
+					{tasks.length} tasks
+				</div>
+			</div>
 		</div>
 	);
 };
