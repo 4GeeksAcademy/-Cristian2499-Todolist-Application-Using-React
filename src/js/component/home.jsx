b2ed75b42f1b6bf5b3fdb68e2733c3676a705be6
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-05-30 15:45:45 +0000

    correction

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,18 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [task, setTask] = useState ("");
-	const [tasks, setTasks] = useState ([]);
-	
-	function addTask (e) {
-		//setTasks([...tasks, task])
-		if (e.key === "Enter") {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	function addTask(e) {
+		//se agrega && tasl !== "" esto se hace es no permitir agregar una tarea si el input esta vacio. 
+		if (e.key === "Enter" && task !== "") {
 			setTasks([...tasks, task])
 		}
 	};
 
-	function removeTask (itemToRemove) {
-		setTasks(tasks.filter((item) => item != itemToRemove))
+	function removeTask(id) {
+		setTasks(tasks.filter((item, index) => index != id))
 	};
 
 	return (
@@ -24,19 +24,19 @@ const Home = () => {
 			<h1 className="text-center mt-5">Todolist</h1>
 			<div className="card">
 				<div className="input-group">
-					<input onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => addTask(e)} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="What needs to be done"/>
+					<input onChange={(e) => setTask(e.target.value)} onKeyDown={(e) => addTask(e)} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="What needs to be done" />
 				</div>
 				<div className="ms-3">
 					{tasks.map((item, index) => {
 						return (
 							<div className="d-flex justify-content-between" key={item + index}>
 								<li>{item}</li>
-								<button className="btn me-3" onClick={() => removeTask(item)} >
+								<button className="btn me-3" onClick={() => removeTask(index)} >
 									<i className="fas fa-window-close text-danger"></i>
 								</button>
 							</div>
 						)
-						
+
 					})}
 				</div>
 				<div className="card-footer">
